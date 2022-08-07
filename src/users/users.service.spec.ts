9009148ebb658a26feec37d311bf502d70672a4a
hsimao <e087754958@gmail.com>
2022-08-07 15:47:16 +0800

    test: users service test createAccount, if user exists

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -4,6 +4,7 @@ import { JwtService } from './../jwt/jwt.service';
 import { UsersService } from './users.service';
 import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 const mockRepository = {
   findOne: jest.fn(),
@@ -20,8 +21,12 @@ const mockEmailService = {
   sendVerificationEmail: jest.fn()
 };
 
+// 將 Repository 所有內建方法 mock
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
 describe('UserService', () => {
   let service: UsersService;
+  let usersRepository: MockRepository<User>;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
@@ -47,13 +52,34 @@ describe('UserService', () => {
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    usersRepository = module.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it.todo('createAccount');
+  describe('createAccount', () => {
+    it('should fail if user exists', async () => {
+      // mock findOne 返回值
+      usersRepository.findOne.mockResolvedValue({
+        id: 1,
+        email: 'mock@gmail.com'
+      });
+
+      const result = await service.createAccount({
+        email: '',
+        password: '',
+        role: 0
+      });
+
+      expect(result).toMatchObject({
+        ok: false,
+        error: 'There is a user with that email already'
+      });
+    });
+  });
+
   it.todo('login');
   it.todo('findById');
   it.todo('editProfile');
